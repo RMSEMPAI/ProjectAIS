19f45a6dff24dd9d8d7a2beb9b1c964a10d6dc5c
Степанов Иван <stepanovivan548@gmail.com>
2025-10-16 17:04:15 +0700

    Начата реализация нового слоя

--- LogicLab/IDomainObject.cs
+++ LogicLab/IDomainObject.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LogicLab
 {
-    internal interface IDomainObject
+    public interface IDomainObject
     {
     }
 }
--- LogicLab/ITEmployee.cs
+++ LogicLab/ITEmployee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogicLab;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LogicLib
 {
-    public class ITEmployee : IDomain
+    public class ITEmployee : IDomainObject
     {
         public int Id { get; set; }
         public string FullName { get; set; }
