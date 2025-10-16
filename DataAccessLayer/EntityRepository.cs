c18b676d365a72d799a8014b8a1658bab06c419b
Степанов Иван <stepanovivan548@gmail.com>
2025-10-16 19:11:10 +0700

    Добавление новых классов, для реализации

---
+++ DataAccessLayer/EntityRepository.cs
@@ -0,0 +1,42 @@
+﻿using LogicLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    internal class EntityRepository<T> : IRepository<T> where T : IDomainObject
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ReadById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
