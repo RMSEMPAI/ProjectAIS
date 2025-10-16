ec6957e8ddde12766a2dd1638020818e7fcb8c68
Степанов Иван <stepanovivan548@gmail.com>
2025-10-16 16:37:52 +0700

    Создание сущности

---
+++ LogicLab/ITEmployee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLib
+{
+    public class ITEmployee : IDomainObject
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public Position Position { get; set; }
+        public Department Department { get; set; }
+        public decimal Salary { get; set; }
+        public int ExperienceYears { get; set; }
+
+        // Конструктор
+
+        public ITEmployee(int id, string fullName, Position position, Department department, decimal salary, int experienceYears)
+        {
+            Id = id;
+            FullName = fullName;
+            this.Position = position;
+            this.Department = department;
+            Salary = salary;
+            ExperienceYears = experienceYears;
+        }
+        public ITEmployee()
+        {
+
+        }
+    }
+    /// <summary>
+    /// Отделы
+    /// </summary>
+    public enum Department
+    {
+        Unknown = 0,
+        Аналитик,
+        Кибербезопасность,
+        Информационная_структура,
+        Разработчик,
+        ERP_платформы,
+        База_данных
+    }
+    /// <summary>
+    /// Проф. развитие
+    /// </summary>
+    public enum Position
+    {
+        Unknown = 0,
+        Junior,
+        Middle,
+        Senior
+    }
+}
